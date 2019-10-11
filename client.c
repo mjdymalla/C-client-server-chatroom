118a675d6ddb5e631f161a2f5640a73d6c4253dd
ubuntu <markus.dymalla@gmail.com>
2019-10-11 19:02:38 +1000

    working server/client connection with cmd line arguments

--- client.c
+++ client.c
@@ -4,25 +4,53 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <errno.h> 
+#include <string.h> 
+#include <netdb.h> 
+
+// DEFAULT PORT 
+#define default_port 12345
+
+#define MAXDATASIZE 100
 
 int main(int argc, char *argv[]) {
     
     // CREATE A SOCKET
-    int network_socket;
-    network_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int network_socket, port;
+    char buffer[MAXDATASIZE];
+    struct hostent *he;
+    ;
+
+    // CHECK COMMAND LINE ARGUMENTS
+    if (argc < 2) {
+        fprintf(stderr, "usage: client hostname\n");
+        exit(1);
+    }
+    if (argc > 2) {
+        port = atoi(argv[2]);
+    } else port = default_port;
+
+    if ((he=gethostbyname(argv[1])) == NULL) {
+        herror("gethostbyname");
+        exit(1);
+    }
+
+    if ((network_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
 
     // SPECIFYING THE ADDRESS FOR THE SOCKET - SERVER
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
-    server_address.sin_addr.s_addr = INADDR_ANY;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr = *((struct in_addr *)he->h_addr);
 
     // CONNECTION STATUS
-    int connection_status = connect(network_socket, (struct sockaddr*) &server_address, sizeof(server_address));
-
-    // CHECK FOR ERROR WITH THE CONNECTION
-    if (connection_status== -1) {
-        printf("There was an error making a connection to the remote socket \n\n");
+    if (connect(network_socket, (struct sockaddr *)&server_address, \
+    sizeof(struct sockaddr)) == -1) {
+        perror("connect");
+        exit(1);
     }
 
     // RECEIVE DATA FROM THE SERVER
@@ -30,7 +58,7 @@ int main(int argc, char *argv[]) {
     recv(network_socket, &server_response, sizeof(server_response), 0);
 
     // PRINT OUT SERVERS RESPONSE
-    printf("The server sent the data: %s\n\n", server_response);
+    printf("The server sent the data: %s\n", server_response);
 
     // AND THEN CLOSE THE SOCKET
     close(network_socket);
--- server.c
+++ server.c
@@ -14,10 +14,10 @@ int main(int argc, char *argv[]) {
     int server_socket;
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    //DEFINE THE SERVER ADDRESS
+    //DEFINE THE SERVER ADDRESSu
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(9002);
+    server_address.sin_port = htons(12345);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     // BIND THE SOCKET TO OUR SPECIFIED IP AND PORT
